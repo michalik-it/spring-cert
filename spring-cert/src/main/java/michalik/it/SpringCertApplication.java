eea78b6eae390f20f84951580b5d2348f6580cd7
Kamil Michalik <kamil_michalik@epam.com>
2016-09-13 14:18:00 +0200

    Initial commit

---
+++ spring-cert/src/main/java/michalik/it/SpringCertApplication.java
@@ -0,0 +1,12 @@
+package michalik.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCertApplication.class, args);
+	}
+}
